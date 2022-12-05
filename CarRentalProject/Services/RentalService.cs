a6feb7242543f0a58d15bcb69aa4eeebb22e4549
Asael Godoy <asael.godoy00@gmail.com>
2022-12-05 01:00:26 -0300

    Primeira soução (sem Interface)

---
+++ CarRentalProject/Services/RentalService.cs
@@ -0,0 +1,39 @@
+﻿
+using CarRentalProject.Entities;
+
+namespace CarRentalProject.Services
+{
+    internal class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+
+            double basicPayment = 0.0;
+            if (duration.TotalHours > 12.0)
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+            else
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);                
+            }
+
+            double tax = _brazilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+
+        }
+    }
+}
